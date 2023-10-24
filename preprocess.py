6c673aa58654af67cedc34559697efb6ead2269a
Rayjine <65818863+Rayjine@users.noreply.github.com>
2023-10-24 17:53:52 +0200

    cleaning of run.ipynb

--- preprocess.py
+++ preprocess.py
@@ -209,3 +209,18 @@ def standardize(x):
     std_x = np.std(x)
     x = x / std_x
     return x, mean_x, std_x
+
+def standardize_std(x):
+    """
+    Standardize the columns of x, might do some other processing after
+        
+    Args:
+        x (np.array): data
+        
+    Returns:
+        s_x: standardized matrix 
+    """
+    
+    s_x, mean, std = standardize(x)
+    
+    return s_x
