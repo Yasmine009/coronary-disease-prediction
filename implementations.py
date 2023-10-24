0a502133632c5f80d9b265d037301eb327469031
Rayjine <65818863+Rayjine@users.noreply.github.com>
2023-10-24 17:00:46 +0200

    implementation of knn

--- implementations.py
+++ implementations.py
@@ -287,3 +287,30 @@ def predict(w,X):
     y = sigmoid(z)
     y_pred = np.where(y>=0.5,1,-1)
     return y_pred
+
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def euclidean_distance(self, x1, x2):
+        return np.sqrt(np.sum((x1 - x2) ** 2))
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        # Compute distances between x and all examples in the training set
+        distances = [self.euclidean_distance(x, x_train) for x_train in self.X_train]
+        # Sort by distance and return indices of the first k neighbors
+        k_indices = np.argsort(distances)[:self.k]
+        # Extract the labels of the k nearest neighbor training samples
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        # Return the most common class label
+        most_common = np.bincount(k_nearest_labels).argmax()
+        return most_common
